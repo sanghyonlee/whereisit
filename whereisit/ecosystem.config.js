8305765dc90015007d88c3d8065ffb60da6707e2
sanghyonlee <michin1559@naver.com>
2020-12-10 01:01:16 +0900

    fix error about pm2 in ecosystem.config.js and modify favicon.ico

--- whereisit/ecosystem.config.js
+++ whereisit/ecosystem.config.js
@@ -2,16 +2,11 @@ module.exports = {
   apps: [
     {
       name: 'whereisit',
+      port: 3000,
       exec_mode: 'cluster',
       instances: '1', // Or a number of instances
       script: './node_modules/nuxt/bin/nuxt.js',
       args: 'start'
     }
   ],
-  env: {
-    // 개발 환경설정
-    PORT: 3000,
-    NODE_ENV : "development"
-
-  },
 }
